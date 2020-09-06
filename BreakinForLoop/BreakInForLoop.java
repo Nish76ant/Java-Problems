4347bcdc3eeaaa395f3f13fd63d476307b04b1a3
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-09-06 21:53:23 +0530

    Create BreakInForLoop.java

---
+++ BreakinForLoop/BreakInForLoop.java
@@ -0,0 +1,17 @@
+public class BreakinForLoop 
+{
+    public static void main(String [] args)
+    {
+        for(int i = 1;i<=100;i++)
+        {
+            if(i==35)
+            {
+                break;
+          
+            }
+            System.out.println(i);
+        }
+
+    }
+    
+}
