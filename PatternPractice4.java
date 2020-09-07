43dcbc3f888106de9e45fbc263ba9b0801cd2d57
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-09-07 20:37:16 +0530

    Create PatternPractice4.java
    /*
    Pattern Practice 5 in Java
    
    ********
    *******
    ******
    *****
    ****
    ***
    **
    *
    
    */

---
+++ PatternPractice4.java
@@ -0,0 +1,33 @@
+/*
+Pattern Practice 5 in Java
+
+********
+*******
+******
+*****
+****
+***
+**
+*
+
+*/
+
+import java.util.Scanner;
+public class PatternPractice5 
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n \n");
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=n-i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
