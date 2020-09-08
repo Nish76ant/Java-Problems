42c8ada262de4f18064cdbdbb9153bf5918ce848
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-09-08 19:38:00 +0530

    Create DoWhileLoopPractice1.java

---
+++ DoWhileLoopPractice1.java
@@ -0,0 +1,19 @@
+/*
+do while loop in java
+
+*/
+
+public class DoWhilePractice 
+{
+    public static void main(String [] args)
+    {
+        int n = 0;
+        do
+        {
+            System.out.println(" n is"+n);
+        }
+        while(n != 0);
+
+    }
+    
+}
