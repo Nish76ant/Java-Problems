10b911d56079bbcba4f1f6ea2481732a0519fa02
Nishant kumar <55913847+Nish76ant@users.noreply.github.com>
2020-09-10 20:13:17 +0530

    Create MultiDimensionalArray.java

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,23 @@
+import sun.tools.jstat.SymbolResolutionClosure;
+
+//MultiDimensional Array in Java
+public class MultiDimensionalArray 
+{
+    public static void main(String [] args)
+    {
+        //Multi Dimensional Array in Java
+        //1 Dimensional Array
+        //int a[] = new int[5];
+        //2 Dimensional Array
+        //int a[][] = new int[5][3];
+        //Retrive the value
+        //System.out.println(a[0][0]);
+        //Intialzie the Array
+        //a[1][2] = 10;
+       // System.out.println(a[1][2]);
+
+       //Intialize the value
+       int b[][] = {{3,1,5,7,2},{4,1,7},{1,2,3,5,6,8}};
+       System.out.println(b[2][0]);
+    }  
+}
